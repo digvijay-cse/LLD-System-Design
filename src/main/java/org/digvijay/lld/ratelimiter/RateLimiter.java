5b2da9bb19603bfe53db563eb6755b147963431b
digvijay-cse <108177753+digvijay-cse@users.noreply.github.com>
2024-12-05 22:41:18 +0530

    Update RateLimiter.java

--- src/main/java/org/digvijay/lld/ratelimiter/RateLimiter.java
+++ src/main/java/org/digvijay/lld/ratelimiter/RateLimiter.java
@@ -1,4 +1,4 @@
-package org.digvijay.lld;
+package org.digvijay.lld.ratelimiter;
 
 import java.util.LinkedList;
 import java.util.HashMap;
@@ -31,14 +31,26 @@ public class RateLimiter {
   }
 
   public static void main(String[] args) {
+    // A.
     // 1 requests in every 2 seconds.
-    RateLimiter rateLimiterTier = new RateLimiter(2, 1);
-    System.out.println(rateLimiterTier.isRequestAllowed(1,1)); // 200
-    System.out.println(rateLimiterTier.isRequestAllowed(1,2)); // 429
-    System.out.println(rateLimiterTier.isRequestAllowed(1,3)); // 200
-    System.out.println(rateLimiterTier.isRequestAllowed(2,4)); // 200
-    System.out.println(rateLimiterTier.isRequestAllowed(2,5)); // 429
-    System.out.println(rateLimiterTier.isRequestAllowed(2,6)); // 200
-    System.out.println(rateLimiterTier.isRequestAllowed(3,7)); // 200
+    RateLimiter rateLimitTier_A = new RateLimiter(2, 1);
+    System.out.println(rateLimitTier_A.isRequestAllowed(1,1)); // 200
+    System.out.println(rateLimitTier_A.isRequestAllowed(1,2)); // 429
+    System.out.println(rateLimitTier_A.isRequestAllowed(2,2)); // 200
+    System.out.println(rateLimitTier_A.isRequestAllowed(1,3)); // 200
+    // B.
+    // 1 requests in every 5 seconds.
+    RateLimiter rateLimitTier_B = new RateLimiter(5, 1);
+    System.out.println(rateLimitTier_B.isRequestAllowed(2,1)); // 200
+    System.out.println(rateLimitTier_B.isRequestAllowed(2,5)); // 429
+    System.out.println(rateLimitTier_B.isRequestAllowed(3,3)); // 200
+    // C.
+    // 3 requests in every 2 seconds.
+    RateLimiter rateLimitTier_C = new RateLimiter(2, 3);
+    System.out.println(rateLimitTier_C.isRequestAllowed(3,5)); // 200
+    System.out.println(rateLimitTier_C.isRequestAllowed(3,6)); // 200
+    System.out.println(rateLimitTier_C.isRequestAllowed(3,6)); // 200
+    System.out.println(rateLimitTier_C.isRequestAllowed(3,6)); // 429
+    System.out.println(rateLimitTier_C.isRequestAllowed(4,6)); // 200
   }
 }
